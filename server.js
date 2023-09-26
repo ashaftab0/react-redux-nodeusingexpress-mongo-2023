d6fd7231cf8a47c68a1d61878ca6f856027b0e74
Ashraf Aftab <ashraf.aftab@publicissapient.com>
2023-09-26 15:40:23 +0530

    Route with express Router

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 
+const userRoute = require('./routes/api/users');
+const authRoute = require('./routes/api/auth');
+const profileRoute = require('./routes/api/profile');
+const postsRoute = require('./routes/api/posts');
+
 //Connect Database
 connectDB();
+
+// Define Routes
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/profile', profileRoute);
+app.use('/api/posts', postsRoute);
 
 app.get('/', (re, res) => res.send('API Running Woo'));
 
