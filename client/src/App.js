0b90737c12a77220b16f3350cc34b0910952588e
Ashraf Aftab <ashraf.aftab@publicissapient.com>
2023-10-07 01:35:16 +0530

    git commit -m "React router setup of Register and Logion page"

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import { BrowserRouter as Router, Route, Navigate, Routes} from "react-router-dom";
 
 const App = () =>
-  <>
-    <h1>App</h1>
-  </>
+  <Router>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+      <section className='container'>
+        <Routes>
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path='/login' element={<Login />} />
+        </Routes>
+      </section>
+    </>
+  </Router>
 
 export default App;
